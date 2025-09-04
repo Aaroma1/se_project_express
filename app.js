c32de2b5149cd6213f76c28d7ecb38b637fd830e
Aaron Romano <aaronjromano1@gmail.com>
2025-09-03 18:06:02 -0700

    logging middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { errors } = require("celebrate");
 const mainRouter = require("./routes/index");
 const auth = require("./middlewares/auth");
 const errorHandler = require("./middlewares/error-handler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -21,10 +22,13 @@ app.get("/items", require("./controllers/clothingItems").getItems);
 
 app.use(auth);
 
+app.use(requestLogger);
+
 app.use("/", mainRouter);
 
-app.use(errors());
+app.use(errorLogger);
 
+app.use(errors());
 app.use(errorHandler);
 
 mongoose
